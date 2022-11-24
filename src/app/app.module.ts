0917e67b5660c0d91b4255f776bc0afea1203f21
Tsyriulnikov <max_djvu@mail.ru>
2022-11-24 16:41:27 +0300

    ngrx install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {NetworkInterceptor} from "./interceptors/network.interceptor";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -23,6 +23,7 @@ import { PokedexComponent } from './components/pokedex/pokedex.component';
     PokemonListModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    StoreModule.forRoot({}, {}),
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass:NetworkInterceptor, multi:true}
