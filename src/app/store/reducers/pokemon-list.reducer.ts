6d8b12165ec9e92c00d58354c70461c260a8e5a4
Tsyriulnikov <max_djvu@mail.ru>
2022-11-26 21:33:26 +0300

    ngrx create store

--- src/app/store/reducers/pokemon-list.reducer.ts
+++ src/app/store/reducers/pokemon-list.reducer.ts
@@ -1,1 +1,21 @@
-export function pokemonListReducer()
+import {PokemonResponse} from "../../models/pokemon-list.models";
+
+export interface PokemonState {
+pokemonList:PokemonResponse
+}
+
+const pokemonListIntialState:PokemonState={
+  pokemonList:{
+    count: 0,
+    next: null,
+    previous: null ,
+    results: []
+  }
+}
+
+export function pokemonListReducer(
+  state:State= = initialState,
+  action:
+){
+
+}
